9e2b4b27f6a3a7fa02412786654b3fc8994042c9
Zaicheng Qi <vmlinz@gmail.com>
2016-06-04 19:47:21 +0800

    Using boders to help visualize flexbox layout

--- index.ios.js
+++ index.ios.js
@@ -14,7 +14,6 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
   },
   monitor: {
-    backgroundColor: 'yellow',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'stretch',
@@ -23,17 +22,14 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'red',
   },
   buttons: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-around',
-    backgroundColor: 'green',
   },
   laps: {
-    backgroundColor: 'blue',
     flex: 1,
     justifyContent: 'space-around',
     alignItems: 'center',
@@ -59,21 +55,27 @@ class Stopwatch extends Component {
       </View>
     );
   }
+  border(color) {
+    return {
+      borderColor: color,
+      borderWidth: 4,
+    };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <View style={styles.monitor}>
-          <View style={styles.timer}>
+      <View style={[styles.container]}>
+        <View style={[styles.monitor, this.border('yellow')]}>
+          <View style={[styles.timer, this.border('red')]}>
             <Text>
               00:00.00
             </Text>
           </View>
-          <View style={styles.buttons}>
+          <View style={[styles.buttons, this.border('green')]}>
             {this.startStopButton()}
             {this.lapButton()}
           </View>
         </View>
-        <View style={styles.laps}>
+        <View style={[styles.laps, this.border('blue')]}>
           <View style={styles.lap}>
             <Text>
               Lap 1
